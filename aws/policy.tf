a721d2dfcf8df8943cd39396d54ba69ce3464752
Marcin Wyszynski <marcin.pixie@gmail.com>
2018-06-28 12:47:19 +0200

    Remove parameter slash

--- aws/policy.tf
+++ aws/policy.tf
@@ -135,8 +135,8 @@ data "aws_iam_policy_document" "geopoiesis" {
     ]
 
     resources = [
-      "arn:aws:ssm:${var.region}:${local.account_id}:parameter/${var.ssm_prefix}/*",
-      "arn:aws:ssm:${var.region}:${local.account_id}:parameter/${var.ssm_prefix}/*/*",
+      "arn:aws:ssm:${var.region}:${local.account_id}:parameter${var.ssm_prefix}/*",
+      "arn:aws:ssm:${var.region}:${local.account_id}:parameter${var.ssm_prefix}/*/*",
     ]
   }
 
